2888b412e4713d7fb12371a13d21bec7edf82b5d
itsspriyansh <priyanshkp01@gmail.com>
2023-01-30 15:23:00 +0530

    added constraints

--- User.js
+++ User.js
@@ -1,9 +1,33 @@
 const mongoose = require("mongoose")
 
+const addressSchema = new mongoose.Schema({
+    street : String,
+    city : String,
+})
+
 const userSchema = new mongoose.Schema({
     name : String,
-    age : Number,
+    age : {
+        type : Number,
+        min : 1,
+        max : 100,
+    },
+    email : {
+        type : String,
+        required : true,
+    },
+    createdAt : {
+        type : Date,
+        default : () => Date.now(),
+        immutable : true,
+    },
+    updatedAt : {
+        type : Date,
+        default : () => Date.now(),
+    },
+    bestFriend : mongoose.SchemaTypes.ObjectId,
+    hobbies : [String],
+    address : addressSchema,
 })
 
 module.exports = mongoose.model("User", userSchema)
- 
--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const mongoose = require("mongoose")
-const user = require("./User")
-const worker = require("./Worker")
+const User = require("./User")
 
 mongoose.connect("mongodb://localhost/testdb", () => {
     console.log ("connected to database")
 })
+
+addUser = async () => {
+    try {
+        const user = await User.create({
+            name : "Priyansh",
+            age : 21,
+            email : "priyansh@gmail.com",
+            hobbies : ["reading", "dancing", "digial painting"],
+            address : {
+                street : "80 feet road",
+                city : "jaipur"
+            }
+        })
+        console.log (user)
+    } catch (error) {
+        // console.log (error.message)
+    }
+}
+
+// addUser()
